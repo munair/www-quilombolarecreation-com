3eb596c087faeb570883921afe9154b902ffb71e
Munair Simpson <munair@gmail.com>
2014-07-29 22:23:25 +0000

    updated kr.bash

--- kr.bash
+++ kr.bash
@@ -1,20 +1,28 @@
 #!/bin/bash
 # script name : kr.bash
 # 
-git clone git@github.com:munair/www-quilombolarecreation-kr.git
+# git clone git@github.com:munair/www-quilombolarecreation-kr.git
+
+git clone https://github.com/munair/www-quilombolarecreation-kr.git
 cd www-quilombolarecreation-kr
-cp -f ../js/* js
-cp -f ../css/* css
-cp -f ../images/* images
-cp -f ../fonts/* fonts
-cp -f ../sounds/* sounds
-cat ../README.md | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > README.md
-cat ../edit.bash | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > edit.bash
-cat ../inc_contact.html | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > inc_contact.html
-cat ../inc_email.html | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > inc_email.html
-cat ../inc_formconfirmation.html | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > inc_formconfirmation.html
-cat ../package.json | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > package.json
-cat ../web.js | sed 's/-com/-kr/g' | sed 's/\.com/\.kr/g' > web.js
+echo "you have five seconds to Ctrl-Z in your manual changes to the following files:"
+ls
+sleep 5
+/bin/cp ../js/* js
+/bin/cp ../css/* css
+/bin/cp ../images/* images
+/bin/cp ../fonts/* fonts
+/bin/cp ../sounds/* sounds
+cat ../README.md | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > README.md
+cat ../edit.bash | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > edit.bash
+cat ../inc_capoeira.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_capoeira.html
+cat ../inc_music.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_music.html
+cat ../inc_faq.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_faq.html
+cat ../inc_contact.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_contact.html
+cat ../inc_email.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_email.html
+cat ../inc_formconfirmation.html | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > inc_formconfirmation.html
+cat ../package.json | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > package.json
+cat ../web.js | sed 's/quilombolarecreation-com/quilombolarecreation-kr/g' | sed 's/quilombolarecreation\.com/quilombolarecreation\.kr/g' > web.js
 bash edit.bash . "[AUTOMATED] Pushing changes to KR website from COM website excluding index.html in particular. To modify index.html git clone the repo and modify by hand." noprompting
 cd ..
 rm -rf www-quilombolarecreation-kr
